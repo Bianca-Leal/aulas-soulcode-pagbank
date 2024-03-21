7a519fad7c872f38fe4e840154544d59a76dc5ed
Bianca-Leal <bleal8882@gmail.com>
2024-03-20 22:01:36 -0300

    classe animal

---
+++ src/br/com/soulcode/pooparte1/Animal.java
@@ -0,0 +1,31 @@
+package br.com.soulcode.pooparte1;
+
+public class Animal {
+    //atributos
+    private String nome;
+    private int idade;
+
+
+    public Animal(String nome , int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public int getIdade(){
+        return idade;
+    }
+
+
+    public void aniversarioanimal(){
+        idade++;
+    }
+    //esse metodo insere no banco de dados com os atributos nome e idade
+    // o atributo idade deve ser inteiro
+    public void inserenobancodedados(){
+        //conexao com o banco
+
+    }
+
+}
---
+++ src/br/com/soulcode/pooparte1/Main.java
@@ -0,0 +1,29 @@
+package br.com.soulcode.pooparte1;
+public class Main {
+    public static void main(String[] args) {
+        //Criar um objeto
+        Animal animal1 = new Animal("Rafael",10);
+        Animal animal2 = new Animal("RAFAEL 2", 30);
+
+
+        //Mostrar os atributos  e também os metodos
+        System.out.println("O nome do animal é "+ animal1.getNome());
+        System.out.println("A idade do animal é "+ animal1.getIdade());
+        animal1.aniversarioanimal();
+        System.out.println("A nova idade do animal é "+ animal1.getIdade());
+
+
+        System.out.println("=====");
+        System.out.println("nome do animal2 "+ animal2.getNome());
+
+
+        //==========
+
+
+        Animal animal3 = new Animal("rafael 3", 40);
+        System.out.println("=====");
+        System.out.println("nome do animal3 "+ animal3.getNome());
+        animal3.inserenobancodedados();
+
+    }
+}

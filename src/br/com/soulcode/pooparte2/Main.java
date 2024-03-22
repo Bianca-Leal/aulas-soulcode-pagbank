2e53282d48e41fdd452fbc99630cd38142b047d4
Bianca-Leal <bleal8882@gmail.com>
2024-03-21 22:52:50 -0300

    adicao aula pooparte2

---
+++ src/br/com/soulcode/pooparte2/Main.java
@@ -0,0 +1,13 @@
+package br.com.soulcode.pooparte2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa(); //objeto foi criado
+        Pessoa pessoa2 = new Pessoa();
+
+        pessoa1.setNome("Rafael 2");
+        System.out.println(pessoa1);
+
+    }
+}
